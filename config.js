81d0c844404068e9a78596e9df5f417d5585aefb
Sekar Arum <almanosetiawan@gmail.com>
2026-04-24 00:59:35 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDt5tMCf480mDGGeDd7liKuGt3zS2_M5OI",
+  authDomain: "thai-tea-cst-ac9db.firebaseapp.com",
+  projectId: "thai-tea-cst-ac9db",
+  storageBucket: "thai-tea-cst-ac9db.firebasestorage.app",
+  messagingSenderId: "907107728020",
+  appId: "1:907107728020:web:7340be99b3ab4c07cdd281",
+  measurementId: "G-DJFC6NYVD1"
+};
+
+// ✅ Inisialisasi (JANGAN DIHAPUS!)
+firebase.initializeApp(firebaseConfig);
